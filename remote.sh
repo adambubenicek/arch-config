3bcd5914b692d3708f7631aa1140db1f4f580734
Adam Bubenicek <adam@adambubenicek.com>
2024-10-01 15:40:51 +0200

    add output styling

--- remote.sh
+++ remote.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")" || exit
 
+printf -v divider '%80s' '' 
+divider="\e[30m${divider// /-}\e[0m"
+
 function ensure_file() {
   local path="$1"
   local content_path="$2"
 
   if [[ ! -f "$path" ]]; then
-    echo "Creating file '$path'"
+    echo -e "$divider"
+    echo -e "> Creating file '\e[1;37m$path\e[0m'."
   elif ! cmp -s "$path" "$content_path"; then
-    echo "Updating file '$path'"
+    echo -e "$divider"
+    echo -e "> Updating file '\e[1;37m$path\e[0m'."
     diff --color=always "$path" "$content_path"
   fi
 
@@ -20,7 +25,8 @@ function ensure_dir() {
   local mode="$2"
 
   if [[ ! -d "$path" ]]; then
-    echo "Creating directory '$path'"
+    echo -e "$divider"
+    echo -e "> Creating directory '\e[1;37m$path\e[0m'."
     run_command quiet mkdir --mode="$mode" "$path"
   fi
 }
@@ -39,17 +45,26 @@ function ensure_attributes() {
   local existing_group="${existing[2]}"
 
   if [[ "$mode" != "$existing_mode" ]]; then
-    echo "Changing mode of '$path' from '$existing_mode' to '$mode'"
+    echo -e "$divider"
+    echo -en "> Changing mode of '\e[1;37m$path\e[0m' "
+    echo -en "from '\e[1;37m$existing_mode\e[0m' "
+    echo -e "to '\e[1;37m$mode\e[0m'."
     run_command quiet chmod "$mode" "$path"
   fi
 
   if [[ "$owner" != "$existing_owner" ]]; then
-    echo "Changing owner of '$path' from '$existing_owner' to '$owner'"
+    echo -e "$divider"
+    echo -en "> Changing owner of '\e[1;37m$path\e[0m' "
+    echo -en "from '\e[1;37m$existing_owner\e[0m' "
+    echo -e "to '\e[1;37m$owner\e[0m'."
     run_command quiet chown "$owner" "$path"
   fi
 
   if [[ "$group" != "$existing_group" ]]; then
-    echo "Changing group of '$path' from '$existing_group' to '$group'"
+    echo -e "$divider"
+    echo -en "> Changing group of '\e[1;37m$path\e[0m' "
+    echo -en "from '\e[1;37m$existing_group\e[0m' "
+    echo -e "to '\e[1;37m$group\e[0m'."
     run_command quiet chgrp "$group" "$path"
   fi
 }
@@ -65,17 +80,26 @@ function run_command() {
   local command="$*"
 
   if [[ "$quiet" == false ]]; then 
-    echo "Running '$command'" 
+    echo -e "$divider"
+    echo -e "> Running '\e[1;37m$command\e[0m'" 
   fi
 
   while ! eval "$command"; do
     while true; do
-      echo -n "Running '$command' failed, continue? [Yn]: " 
+      echo -e "$divider"
+      echo -en "\e[31m>\e[0m Running '\e[1;37m$command\e[0m' failed. What do? " 
+      echo -en "\e[30m[\e[0m"
+      echo -en "\e[1ms\e[0mkip"
+      echo -en "\e[30m|\e[0m"
+      echo -en "\e[1mr\e[0metry"
+      echo -en "\e[30m|\e[0m"
+      echo -en "\e[1me\e[0mxit"
+      echo -en "\e[30m] \e[0m"
       read -r answer
       case "$answer" in
-        Y|y|'') break 2;;
-        N|n) exit 1;;
-        *) continue;;
+        s|skip) break 2;;
+        r|retry) continue 2;;
+        e|exit) exit 1;;
       esac
     done
   done
