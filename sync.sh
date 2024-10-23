4d9a92c7d5efc48e959bcd2f2861561569fbd174
Adam Bubenicek <adam@adambubenicek.com>
2024-10-23 15:06:39 +0200

    sync to a single host at a time

--- sync.sh
+++ sync.sh
@@ -127,46 +127,40 @@ while getopts "b:" opt; do
 done
 shift $((OPTIND-1))
 
-if (( "$#" > 0 )); then
-  hosts=( "$@" )
-else
-  hosts=( hippo kangaroo owl sloth )
-fi
+ssh_host="${1:-localhost}"
 
 printf -v divider '%80s' '' 
 divider="\e[2m${divider// /=}\e[0m"
 
-for host in "${hosts[@]}"; do
-  sync_dir="$(mktemp -d)"
+sync_dir="$(mktemp -d)"
 
-  cp remote.sh "$sync_dir/remote.sh"
+cp remote.sh "$sync_dir/remote.sh"
 
-  ssh_opts=(
-   -o ControlMaster=auto
-   -o ControlPath=/root/.ssh/%C
-   -o ControlPersist=60
+ssh_opts=(
+ -o ControlMaster=auto
+ -o ControlPath=/root/.ssh/%C
+ -o ControlPersist=60
+)
+
+HOSTNAME=$(ssh "${ssh_opts[@]}" "$ssh_host" uname -n)
+
+echo -e "$divider"
+echo -en "> Syncing to '\e[1;37m$HOSTNAME\e[0m' "
+echo -e "during its '\e[1;37m$boot\e[0m' boot."
+
+source ./config.sh
+
+if [[ $boot != "regular" ]]; then
+  ssh_opts+=(
+    -o StrictHostKeyChecking=off
+    -o UserKnownHostsFile=/dev/null 
   )
+fi
 
-  HOSTNAME=$(ssh "${ssh_opts[@]}" "$host" uname -n)
+remote_sync_dir=$(ssh "${ssh_opts[@]}" "$ssh_host" mktemp -d)
 
-  echo -e "$divider"
-  echo -en "> Syncing to '\e[1;37m$HOSTNAME\e[0m' "
-  echo -e "during its '\e[1;37m$boot\e[0m' boot."
+scp "${ssh_opts[@]}" -q "$sync_dir"/* "$ssh_host:$remote_sync_dir" 
+ssh "${ssh_opts[@]}" "$ssh_host" "$remote_sync_dir"/remote.sh || true
+ssh "${ssh_opts[@]}" "$ssh_host" rm -rf "$remote_sync_dir"
 
-  source ./config.sh
-
-  if [[ $boot != "regular" ]]; then
-    ssh_opts+=(
-      -o StrictHostKeyChecking=off
-      -o UserKnownHostsFile=/dev/null 
-    )
-  fi
-
-  remote_sync_dir=$(ssh "${ssh_opts[@]}" "$host" mktemp -d)
-  
-  scp "${ssh_opts[@]}" -q "$sync_dir"/* "$host:$remote_sync_dir" 
-  ssh "${ssh_opts[@]}" "$host" "$remote_sync_dir"/remote.sh || true
-  ssh "${ssh_opts[@]}" "$host" rm -rf "$remote_sync_dir"
-
-  rm -rf "$sync_dir"
-done
+rm -rf "$sync_dir"
