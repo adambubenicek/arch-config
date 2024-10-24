4cf1a4647c1ccb57093f421f3ae9c2fc29a5d136
Adam Bubenicek <adam@adambubenicek.com>
2024-10-24 11:59:28 +0200

    separate local functions

---
+++ local.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+
+function f() {
+  if [[ " ${f_boots[*]} " != *" $boot "* ]]; then
+    return 0 
+  fi
+
+  local src_path
+  local dest_path
+  local template
+  local mode
+  local owner
+  local group
+
+  src_path="$(uuidgen)" 
+  dest_path="$1"
+  shift
+
+  OPTIND=1
+  while getopts "tm:o:g:" opt; do
+    case "$opt" in
+      t) template=true;;
+      m) mode=$OPTARG;;
+      o) owner=$OPTARG;;
+      g) group=$OPTARG;;
+      *) break;;
+    esac
+  done
+
+  template=${template:-false}
+  mode=${mode:-644}
+  owner=${owner:-root}
+  group=${group:-$owner}
+
+  if [[ "$template" == true ]]; then
+    local script=""
+
+    while IFS= read -r line; do
+      if [[ "$line" =~ [^[:space:]]*%%[[:space:]]*(.*)$ ]]; then
+        script+="${BASH_REMATCH[1]}"$'\n'
+      elif [[ "$line" =~ [^[:space:]]*%=[[:space:]]*(.*)$ ]]; then
+        script+="echo \"${BASH_REMATCH[1]}\""$'\n'
+      else
+        script+="echo '${line//\'/\'\"\'\"\'}'"$'\n'
+      fi
+    done < ".$dest_path"
+
+    eval "$script" > "$sync_dir/$src_path"
+  else
+    cat ".$dest_path" > "$sync_dir/$src_path"
+  fi
+  
+  if [[ "$boot" == "install-chroot" ]]; then
+    dest_path="/mnt$dest_path"
+  fi
+
+  {
+    echo ensure_file "$dest_path" "./$src_path"
+    echo ensure_attributes "$dest_path" "$mode" "$owner" "$group"
+  } >> "$sync_dir/remote.sh"
+}
+
+function d() {
+  if [[ " ${d_boots[*]} " != *" $boot "* ]]; then
+    return 0 
+  fi
+
+  local dest_path
+  local mode
+  local owner
+  local group
+
+  dest_path="$1"
+  shift
+
+  OPTIND=1
+  while getopts "m:o:g:" opt; do
+    case "$opt" in
+      m) mode=$OPTARG;;
+      o) owner=$OPTARG;;
+      g) group=$OPTARG;;
+      *) break;;
+    esac
+  done
+
+  mode=${mode:-755}
+  owner=${owner:-root}
+  group=${group:-$owner}
+  
+  if [[ "$boot" == "install-chroot" ]]; then
+    dest_path="/mnt$dest_path"
+  fi
+
+  {
+    echo ensure_dir "$dest_path" "$mode"
+    echo ensure_attributes "$dest_path" "$mode" "$owner" "$group"
+  } >> "$sync_dir/remote.sh"
+}
+
+function c() {
+  if [[ " ${c_boots[*]} " != *" $boot "* ]]; then
+    return 0 
+  fi
+
+  cmd="$*" 
+
+  if [[ "$boot" == "install-chroot" ]]; then
+    cmd="arch-chroot /mnt $cmd"
+  fi
+
+  echo "run_command $cmd" >> "$sync_dir/remote.sh"
+}
