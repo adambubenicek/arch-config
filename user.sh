dfd4e522b97a3492ae3e2d43bd0dba7b841e2ea2
Adam Bubenicek <adam@adambubenicek.com>
2024-11-15 21:21:25 +0100

    add firefox

--- user.sh
+++ user.sh
@@ -5,6 +5,11 @@ source <(sudo cat .env)
 source local.sh
 
 echo "$REMOTE_HOSTNAME"
+d root root 755 /usr/lib64/firefox/
+f root root 644 /usr/lib64/firefox/firefox.cfg
+d root root 755 /usr/lib64/firefox/defaults/
+d root root 755 /usr/lib64/firefox/defaults/pref
+f root root 644 /usr/lib64/firefox/defaults/pref/autoconfig.js
 
 d adam adam 700 /home/adam/.ssh
 f adam adam 644 /home/adam/.ssh/authorized_keys
