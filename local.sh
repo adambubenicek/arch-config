511be0312e4e53bed1e8817ae39995c334a144f1
Adam Bubenicek <adam@adambubenicek.com>
2024-11-15 20:01:00 +0100

    move more logic into local.sh

--- local.sh
+++ local.sh
@@ -1,36 +1,44 @@
 #!/usr/bin/env bash
 
-SCRIPT=$(< remote.sh)
-SCRIPT+=$'\n'
+script=$(< remote.sh)
+script+=$'\n'
+
+ssh_opts=(
+ -o ControlMaster=auto
+ -o ControlPath=~/.ssh/%C
+ -o ControlPersist=60
+ "$SSH_USER@$SSH_HOST"
+)
+  
+export REMOTE_HOSTNAME
+REMOTE_HOSTNAME=$(ssh "${ssh_opts[@]}" uname -n)
 
 f() {
   path="$4"
 
-  script=""
+  content=""
   while IFS= read -r line; do
     if [[ "$line" =~ [^[:space:]]*%%[[:space:]]*(.*)$ ]]; then
-      script+="${BASH_REMATCH[1]}"$'\n'
+      content+="${BASH_REMATCH[1]}"$'\n'
     elif [[ "$line" =~ [^[:space:]]*%=[[:space:]]*(.*)$ ]]; then
-      script+="echo \"${BASH_REMATCH[1]}\""$'\n'
+      content+="echo \"${BASH_REMATCH[1]}\""$'\n'
     else
-      script+="echo '${line//\'/\'\"\'\"\'}'"$'\n'
+      content+="echo '${line//\'/\'\"\'\"\'}'"$'\n'
     fi
   done < ".$path"
 
-  content_encoded=$(eval "$script" | base64 --wrap=0)
-  SCRIPT+="f $* $content_encoded"$'\n'
+  content_encoded=$(eval "$content" | base64 --wrap=0)
+  script+="f $* $content_encoded"$'\n'
 }
 
 d() {
-  SCRIPT+="d $*"$'\n'
+  script+="d $*"$'\n'
 }
 
 c() {
-  SCRIPT+="c $*"$'\n'
+  script+="c $*"$'\n'
 }
 
 run() {
-  user="$1"
-  host="$2"
-  ssh "$user@$host" bash -c "'$SCRIPT'"
+  ssh "${ssh_opts[@]}" bash -c "'$script'"
 }
--- user.sh
+++ user.sh
@@ -1,10 +1,12 @@
-ssh_host="${1:-localhost}"
-ssh_user="adam"
+export SSH_HOST="${1:-localhost}"
+export SSH_USER="adam"
 
 source <(sudo cat .env)
 source local.sh
 
+echo "$REMOTE_HOSTNAME"
+
 d adam adam 700 /home/adam/.ssh
 f adam adam 644 /home/adam/.ssh/authorized_keys
 
-run adam 10.98.217.121
+run
