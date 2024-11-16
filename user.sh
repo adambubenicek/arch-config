13cec10d689ce3dd08914590902a6dd7fd9a9eaf
Adam Bubenicek <adam@adambubenicek.com>
2024-11-16 08:25:54 +0100

    add more packages and files

--- user.sh
+++ user.sh
@@ -47,18 +47,38 @@ while  [[ " ${known_hosts[*]} " != *" ${remote_host} "* ]]; do
 done
 
 c sudo hostnamectl set-hostname --static "$remote_host"
+
+c dnf install -y \
+  "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-\$(rpm -E %fedora).noarch.rpm" \
+  "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-\$(rpm -E %fedora).noarch.rpm"
+
+c dnf swap -y mesa-va-drivers mesa-va-drivers-freeworld
+c dnf swap -y mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+c dnf swap -y ffmpeg-free ffmpeg --allowerasing
+
 c dnf install -y \
   ripgrep \
   neovim \
   fzf \
   nodejs \
-  shellcheck
+  shellcheck \
+  gimp \
+  inkscape \
+  blender \
+  steam
 
 c npm install -g \
   bash-language-server \
   typescript-language-server \
   svelte-language-server \
   prettier
+
+f root root 644 /etc/hosts
+
+d root root 700 /root/.ssh
+f root root 644 /root/.ssh/authorized_keys
+f root root 600 /root/.ssh/id_ed25519
+f root root 644 /root/.ssh/id_ed25519.pub
 
 d root root 755 /usr/lib64/firefox/
 f root root 644 /usr/lib64/firefox/firefox.cfg
@@ -68,6 +88,8 @@ f root root 644 /usr/lib64/firefox/defaults/pref/autoconfig.js
 
 d adam adam 700 /home/adam/.ssh
 f adam adam 644 /home/adam/.ssh/authorized_keys
+f adam adam 600 /home/adam/.ssh/id_ed25519
+f adam adam 644 /home/adam/.ssh/id_ed25519.pub
 d adam adam 755 /home/adam/.bashrc.d
 f adam adam 644 /home/adam/.bashrc.d/overrides
 d adam adam 755 /home/adam/.config/git
